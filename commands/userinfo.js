1d320b890f987913e2dd587465fe796d27f3825c
jettbui <jettcbui@gmail.com>
2020-08-16 15:58:11 -0700

    Implemented userinfo command

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,3 +1,7 @@
+const Discord = require("discord.js");
+const moment = require("moment");
+const momentTz = require("moment-timezone");
+
 module.exports = {
 	name: "userinfo",
     description: "Sends information about a user",
@@ -7,19 +11,28 @@ module.exports = {
     usage: "[user]",
     cooldown: 5,
     guildOnly: false,
-	execute(message, args) { // TODO
+    execute(message, args) {
+
+        const user = (args[0]) ? message.mentions.users.first() : message.author;
+        const member = message.guild.member(user);
+
+        // validity checks
+        if (!member) return message.channel.send("That user does not exist.");
+
         const userEmbed = new Discord.MessageEmbed()
-            .setColor("USER ROLE COLOR")
-            .setTitle("User Info - USERNAME")
-            .setThumbnail("USER ICON URL")
-            .setFooter(informationalEmbed.footer.text)
+            .setColor(member.displayHexColor)
+            .setTitle(`User Info - ${user.tag}`)
+            .setThumbnail(user.displayAvatarURL())
+            .setFooter("Copyright JettBot, © 2020")
             .setTimestamp()
             .addFields(
-                { name: "ID", value: "" },
-                { name: "Display Name", value: "" },
-                { name: "Account Created", value: "" },
-                { name: "Joined", value: "" },
-                { name: "Roles", value: "" }
+                { name: "ID", value: member.id },
+                { name: "Display Name", value: member.displayName },
+                { name: "Account Created",
+                  value: moment.tz(user.createdAt, "America/Los_Angeles").format("MMMM DD, YYYY [at] h:mm A [(PST)]") },
+                { name: "Joined",
+                  value: moment.tz(member.joinedAt, "America/Los_Angeles").format("MMMM DD, YYYY [at] h:mm A [(PST)]") },
+                //{ name: "Roles", value: member.roles.map((r) => `${r}`). join(", ") }
             );
         
         message.channel.send(userEmbed);
