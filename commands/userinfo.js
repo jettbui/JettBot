feaf86ed326b3ab8b1bec0f099db31fab8f127ab
jettbui <jettcbui@gmail.com>
2020-08-16 16:46:55 -0700

    Refined userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js");
-const moment = require("moment");
-const momentTz = require("moment-timezone");
+const moment = require("moment-timezone");
 
 module.exports = {
-	name: "userinfo",
+    name: "userinfo",
     description: "Sends information about a user",
     category: "utility",
-    aliases: [],
+    aliases: ["user"],
     args: false,
     usage: "[user]",
     cooldown: 5,
@@ -19,22 +18,32 @@ module.exports = {
         // validity checks
         if (!member) return message.channel.send("That user does not exist.");
 
+        // get user roles
+        const userRoles = [];
+        member.roles.cache.forEach((r) => {
+            if (r != message.guild.roles.everyone.id) userRoles.push(r);
+        });
+
         const userEmbed = new Discord.MessageEmbed()
-            .setColor(member.displayHexColor)
+            .setColor((member.displayHexColor != "#000000") ? member.displayHexColor : "#969c9f")
             .setTitle(`User Info - ${user.tag}`)
             .setThumbnail(user.displayAvatarURL())
             .setFooter("Copyright JettBot, © 2020")
             .setTimestamp()
             .addFields(
-                { name: "ID", value: member.id },
-                { name: "Display Name", value: member.displayName },
-                { name: "Account Created",
-                  value: moment.tz(user.createdAt, "America/Los_Angeles").format("MMMM DD, YYYY [at] h:mm A [(PST)]") },
-                { name: "Joined",
-                  value: moment.tz(member.joinedAt, "America/Los_Angeles").format("MMMM DD, YYYY [at] h:mm A [(PST)]") },
-                //{ name: "Roles", value: member.roles.map((r) => `${r}`). join(", ") }
+                { name: "Display Name", value: member.displayName, inline: true },
+                { name: "ID", value: member.id, inline: true },
+                {
+                    name: "Account Created",
+                    value: moment.tz(user.createdAt, "America/Los_Angeles").format("MMMM DD, YYYY [at] h:mm A [(PST)]")
+                },
+                {
+                    name: "Joined",
+                    value: moment.tz(member.joinedAt, "America/Los_Angeles").format("MMMM DD, YYYY [at] h:mm A [(PST)]")
+                },
+                { name: "Roles", value: (userRoles.length > 0) ? userRoles.join(", ") : "None" }
             );
-        
+
         message.channel.send(userEmbed);
-	},
+    },
 };
