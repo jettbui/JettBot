834c90e9f707674407c4e1a470b34b3d989c67aa
jettbui <jettcbui@gmail.com>
2020-09-07 18:46:26 -0700

    Status improvements

--- commands/status.js
+++ commands/status.js
@@ -1,14 +1,26 @@
 module.exports = {
     name: "status",
-    description: "Change the status of the bot (developer only)",
+    description: "Change the bot status (developer only)",
     aliases: [],
     args: true,
-    usage: "<status>",
+    usage: "<type> <status>",
     guildOnly: false,
     devOnly: true,
     execute(message, args) {
+        let type = parseInt(args.shift());
         const status = args.join(" ");
-        message.client.user.setActivity(status, { type: "PLAYING" });
+
+        // validity checks
+        if (isNaN(type)) return message.channel.send("Type must be a number.");
+
+        switch (type) {
+            case 0: type = "PLAYING"; break;
+            case 1: type = "LISTENING"; break;
+            case 2: type = "WATCHING"; break;
+            default: type = "PLAYING";
+        }
+
+        message.client.user.setActivity(status, { type: type });
         return message.channel.send(`Status changed to '${status}'`);
     },
 };
